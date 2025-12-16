dc5e5956e7b56f48263891a41eb3af60a720ad21
Yaşar Baran Yiğit <yigitbaran152@gmail.com>
2025-12-16 10:24:08 +0300

    Update education.js

--- models/education.js
+++ models/education.js
@@ -1,16 +1,21 @@
-const db = require('./db');
+const db = require('./db'); // db.js zaten dummy DB olacak
 
 class Education {
-   static async getAll() {
-     const [rows] = await db.query('SELECT * FROM education');
-      return rows;
+  static async getAll() {
+    const [rows] = await db.query('SELECT * FROM education');
+    return rows;
   }
-    static async create(data) {
-       await db.query('INSERT INTO education(level, school, year, gpa) VALUES(?,?,?,?)',
-            [data.level, data.school, data.year, data.gpa]);
- }
- static async delete(id) {
-     await db.query('DELETE FROM education WHERE id=?', [id]);
+
+  static async create(data) {
+    await db.query(
+      'INSERT INTO education(level, school, year, gpa) VALUES (?, ?, ?, ?)',
+      [data.level, data.school, data.year, data.gpa]
+    );
   }
+
+  static async delete(id) {
+    await db.query('DELETE FROM education WHERE id = ?', [id]);
+  }
+}
 
 module.exports = Education;
